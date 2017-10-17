11337f14173a6497bce4960cc4739cf7752f6d75
Aditya Joglekar <joglekara1@gmail.com>
2017-10-17 12:31:43 +0530

    Add all files

---
+++ pc_main/train_model.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+# loads the training set saved. trains and saves results and model
+#
+#
+
+import argparse
+import json
+import os
+import pickle
+
+import pandas as pd
+
+from pc_methods.train_classifier import TrainClassifier
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    
+    # get directory in which code is stored
+    parser.add_argument('--load_path',
+                        default='%s/../../output/dataset/' % os.path.dirname(os.path.abspath(__file__)))
+    parser.add_argument('--save_path',
+                        default='%s/../../output/model/' % os.path.dirname(os.path.abspath(__file__))) 
+
+    # Arguments
+    args = parser.parse_args()
+    load_path = '/media/aditya/New Volume/Semester 5/EL EHD/Project/baby_cry_detection-master/pc_main'
+    save_path = '/media/aditya/New Volume/Semester 5/EL EHD/Project/baby_cry_detection-master/pc_main'
+
+    ####################################################################################################################
+    # TRAIN MODEL
+    ####################################################################################################################
+
+    # load training dataframe from the csv file created by train_set.py
+    train_set = pd.read_csv(os.path.join(load_path, 'dataset.csv'),nrows=80) 
+     #train_set = pd.read_csv(os.path.join(load_path, 'binary_dataset.csv'),nrows=80)
+
+    # instantiate trainer and train
+    train_classifier = TrainClassifier()
+    performance, parameters, best_estimator = train_classifier.train(train_set)
+
+    ####################################################################################################################
+    # SAVE
+    ####################################################################################################################
+
+# /usr/local/lib/python2.7/dist-packages
+    # Save performances
+    with open(os.path.join(save_path, 'performance.json'), 'w') as fp:
+        json.dump(performance, fp)
+
+    # Save parameters
+    with open(os.path.join(save_path, 'parameters.json'), 'w') as fp:
+        json.dump(parameters, fp)
+
+    # Save model (which is a python object)
+    with open(os.path.join(save_path, 'model.pkl'), 'wb') as fp:
+        pickle.dump(best_estimator, fp)
+
+if __name__ == '__main__':
+    main()
---
+++ pc_methods/__init__.py
@@ -0,0 +1,49 @@
+import pydub
+import numpy as np
+
+__all__ = [
+    'Reader'
+]
+
+
+class Reader:
+    """
+    Read input audio file for training set
+    file_name: 'path/to/file/filename.ogg'
+    """
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        pass
+
+    def read_audio_file(self):
+        """
+        Read audio file using pydub package. Pydub reads the file exactly as it is (no resampling, etc.)
+
+        :return:
+        * audio_data as numpy.ndarray. A two-dimensional NumPy array is returned, where the channels are stored
+        along the first dimension, i.e. as columns. If the sound file has only one channel, a one-dimensional array is
+        returned.
+        * sr as int. The sample rate of the audio file [Hz]
+        """
+
+        # Create a silent sound of exactly 5 seconds
+        silent_template = pydub.AudioSegment.silent(duration=5000)
+
+        # Read file
+        print 'changed' # debug
+        #sound = pydub.AudioSegment.from_file(self.file_name) changed
+        sound = pydub.AudioSegment.from_file('/media/aditya/New Volume/Semester 5/EL EHD/Project/baby_cry_detection-master/data/101 - Dog/1-30226-A.ogg') 
+        
+        # check /media/aditya/New Volume/Semester 5/EL EHD/Project/baby_cry_detection-master/data/101 - Dog/1-30226-A.ogg
+        
+        # Trim 5 seconds
+        sound_5s = silent_template.overlay(sound[0:5000])
+
+        # Convert to AudioSegment object to array. /!\ should it be float32 ??? /!\
+        audio_data = (np.fromstring(sound_5s._data, dtype="int16") + 0.00000000001) / (0x7FFF + 0.00000000001)
+
+        # Sample rate
+        sr = sound.frame_rate
+
+        return audio_data, sr
